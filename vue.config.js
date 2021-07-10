1690467ff70bf22c92bf44c16eee3dddda2c83f5
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2021-07-10 22:27:03 +0300

    r: source maps disabled

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 module.exports = {
   // https://github.com/neutrinojs/webpack-chain/tree/v4#getting-started
   chainWebpack(config) {
-    config.devtool('source-map');
     config.module.rule('js').test(/^((?!\.worker).)*\.js$/);
     config.module
       .rule('worker-loader')
